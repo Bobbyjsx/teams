5182553b18109b1ada903f2e2fdb346053ff5be5
Bobby <104563882+akarasauce@users.noreply.github.com>
2022-11-19 03:04:49 -0800

    mobile

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Teams from "./pages/Teams/Teams";
-import Blog from "./pages/blog/Blog";
-import Contact from "./pages/contact/Contact";
-import About from "./pages/About/About";
+
 
 export default function App() {
   return (
@@ -12,9 +10,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Teams/>} />
-          <Route path="Blog" element={<Blog />} />
-          <Route path="Contact" element={<Contact />} />
-          <Route path="About" element={<About />} />
         </Route>
       </Routes>
     </BrowserRouter>
